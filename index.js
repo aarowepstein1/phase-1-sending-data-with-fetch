6848de3c6320dea5ed9d32c4f5ef0f9fdd0ba561
Aarow Epstein <aarowepstein1@gmail.com>
2023-01-20 17:44:45 -0700

    commit tree 56394ddfec353fe35420672023fcadfdc958533f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+
+function submitData () {
+    
+   return fetch('http://localhost:3000/users', configurationObj)
+    .then(resp => resp.json())
+    .then(object => document.body.append(object.id))
+    .catch( error => {
+        document.body.append("Unauthorized Access");
+    });
+}
